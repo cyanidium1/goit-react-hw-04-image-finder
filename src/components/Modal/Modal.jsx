1285bfd38fd7f3963876f6d3c6a6c4ad9ca9f065
Fedir <99473719+cyanidium1@users.noreply.github.com>
2023-06-30 19:55:20 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,19 +2,30 @@ import { useEffect } from 'react';
 import React from 'react';
 
 const Modal = ({ closeModal, full }) => {
-  const close = e => {
-    if (e.target === e.currentTarget || e.key === 'Escape') {
-      window.removeEventListener('keydown', close);
+  useEffect(() => {
+    window.addEventListener('keydown', closeOnEscape);
+  }, []);
+
+  const closeOnEscape = e => {
+    if (e.key === 'Escape') {
+      closeModal();
+    }
+  };
+
+  const closeOnClick = e => {
+    if (e.target === e.currentTarget) {
       closeModal();
     }
   };
 
   useEffect(() => {
-    window.addEventListener('keydown', close);
+    return () => {
+      window.removeEventListener('keydown', closeOnEscape);
+    };
   }, []);
 
   return (
-    <div className="Overlay" onClick={close}>
+    <div className="Overlay" onClick={e => closeOnClick(e)}>
       <div className="Modal">
         <img src={full} alt="imageeee" />
       </div>
